3e393c657c82e36933d51345caa250fd187c4b17
Javier Ibañez Vizuete <javieriibanezvizuete@gmail.com>
2025-07-17 17:47:16 +0200

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import "./App.css";
+
+export const App = () => {
+	return (
+		<>
+			<h1>Proyecto Javier Ibáñez Vizuete CV React</h1>
+			<h2>Proyecto Javier Ibáñez Vizuete CV React</h2>
+			<h3>Proyecto Javier Ibáñez Vizuete CV React</h3>
+			<h4>Proyecto Javier Ibáñez Vizuete CV React</h4>
+			<h5>Proyecto Javier Ibáñez Vizuete CV React</h5>
+			<h6>Proyecto Javier Ibáñez Vizuete CV React</h6>
+			<p>Proyecto Javier Ibáñez Vizuete CV React</p>
+		</>
+	);
+};
